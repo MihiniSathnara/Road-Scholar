0b30db8ba6e09522d1b19d81d68a02b2372c1ac3
MihiniSathnara <smihinis@gmail.com>
2025-05-05 08:29:40 +0530

    Initial commit with project file structure

---
+++ src/main/java/com/RoadScholar/RoadScholar/RoadScholarApplication.java
@@ -0,0 +1,13 @@
+package com.RoadScholar.RoadScholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadScholarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RoadScholar/RoadScholar/RoadScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RoadScholar.RoadScholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
